729ab4d043d051f86dadffe6c0a4764e6eaec9f6
adleadler06-boop <adleadler06@gmail.com>
2026-05-11 22:58:56 +0500

    Create AboutUs.jsx
    function AboutUs() {
      return (
        <div>
          <h1>About Paradise Nursery</h1>
    
          <p>
            Paradise Nursery provides high quality indoor and outdoor plants
            for homes and offices.
          </p>
        </div>
      );
    }
    
    export default AboutUs;

---
+++ src/AboutUs.jsx
@@ -0,0 +1,14 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery provides high quality indoor and outdoor plants
+        for homes and offices.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
